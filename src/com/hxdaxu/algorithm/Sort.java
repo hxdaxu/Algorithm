1728c472b23861fe298a85561b87a8ae69e25e9f
huangqingchun <huangqingchun@gionee.com>
2017-12-05 19:39:58 +0800

    quick sort

--- src/com/hxdaxu/algorithm/Sort.java
+++ src/com/hxdaxu/algorithm/Sort.java
@@ -4,7 +4,7 @@ import com.hxdaxu.algorithm.utils.CommonUtils;
 
 public class Sort {
 	
-	static int compareCount = 0;
+	static int count = 0;
 
 	/**
 	 * @param args
@@ -13,8 +13,8 @@ public class Sort {
 		int [] array = {6,24,85,63,21,2,54,78,5,46,9,32,15,47,85,365,86,6,987,4,5,24};
 		int [] array2 = {1,2,3,4,5,6,7,8,9,12,11,10,13,14,15,16,17,18,19,20};
 		
-		CommonUtils.traverseArray(bubbleSort(array2));
-		System.out.println(compareCount);
+		quickSort(array);
+		CommonUtils.traverseArray(array);
 	}
 	
 	/**
@@ -28,7 +28,6 @@ public class Sort {
 			change = false;
 			
 			for (int j = 0; j < i; ++j) {
-				compareCount++;
 				if (array[j] > array[j+1]) {
 					int t = array[j];
 					array[j] = array[j+1];
@@ -47,19 +46,28 @@ public class Sort {
 	 * 快速排序，是对冒泡排序的改进。
 	 * 平均时间复杂度为O(nlog n),最坏情况下为O(n2)
 	 */
-	public static int[] quickSort(int []array){
-		int low = 0,hight = array.length;
+	public static int partition(int []array,int low,int hight){
 		int pivot = array[low];
-		
 		while(low < hight) {
-			while (low < hight) if(array[hight] >= pivot) hight--;
+			while (low < hight && array[hight] >= pivot) hight--;
 			array[low] = array[hight];
-			while (low < hight) if(array[low] <= pivot) low++;
+			while (low < hight && array[low] <= pivot) low++;
 			array[hight] = array[low];
 		}
 		array[low] = pivot;
-		
-		return array;
+		return low;
+	}
+	
+	public static void QSort(int []array,int low,int hight) {
+		if (low < hight) {
+			int pivotIndex = partition(array, low, hight);
+			QSort(array, low, pivotIndex-1);
+			QSort(array, pivotIndex+1, hight);
+		}
+	}
+	
+	public static void quickSort(int []array) {
+		QSort(array,0,array.length-1);
 	}
 	
 	
